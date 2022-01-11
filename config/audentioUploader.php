8462b8f902fbb9d4af33c91144d482f852a87c75
Jake Booher <jake.booher@gmail.com>
2022-01-11 13:48:04 -0600

    Support for animated GIFs

--- config/audentioUploader.php
+++ config/audentioUploader.php
@@ -4,4 +4,5 @@ return [
     'uploadModel' => \Audentio\LaravelUploader\Models\Upload::class,
     'uploadRoute' => '/upload',
     'uploadGraphQLType' => null,
+    'useImagick' => (bool) env('AUDENTIO_UPLOADER_IMAGICK', false),
 ];
